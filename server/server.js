ce81de0ec70d2590b7cb8ca333f84b57f8dfccdf
your github name <your github mail>
2023-11-18 01:03:41 +0100

    modifying the cors policy

--- server/server.js
+++ server/server.js
@@ -22,6 +22,15 @@ const corsOption = {
   origin: "https://contact-frontend-gub8.onrender.com/",
 };
 app.use(cors(corsOption));
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://contact-frontend-gub8.onrender.com/"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 5000;
